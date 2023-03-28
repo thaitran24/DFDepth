0725e55ad8df9f5feb8e738dc6b4ff01a5d288fe
thaitran24 <thaitran5307@gmail.com>
2023-03-28 12:19:19 +0700

    update train day

--- options.py
+++ options.py
@@ -34,7 +34,7 @@ class MonodepthOptions:
         self.parser.add_argument("--split",
                                  type=str,
                                  help="which training split to use",
-                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "oxford_all"],
+                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "oxford_all", "oxford_day"],
                                  default="eigen_zhou")
         self.parser.add_argument("--num_layers",
                                  type=int,
